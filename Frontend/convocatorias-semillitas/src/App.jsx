7d07236e4de522eb04bfda13a7fed016f785c163
Isabela1994 <florissa172@gmail.com>
2025-08-29 16:58:46 -0500

    nueva actualizacion

--- Frontend/convocatorias-semillitas/src/App.jsx
+++ Frontend/convocatorias-semillitas/src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-
+import AgregarUsuario from "./componentes/formularios/AggrUsuario1";
+//import RegistrarConvocatorias from './componentes/formularios/RegistrarConvocatorias';
 
 function App() {
-
-
   return (
-    <>
-
-    </>
-  )
+    <div className="App">
+      <h1>Gestión de Convocatorias</h1>
+      <AgregarUsuario />
+    </div>
+  );
 }
 
-export default App
+export default App;
