8a9c84d8212f1ded6805f6dacdec7654c5f4f4a3
JoTaZem <jhonfredyzema2@gmail.com>
2025-08-29 16:20:59 -0500

    holaasss

---
+++ Backend/ProyectoConvocatorias/appConvocatoriasSena/viewsLider.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from appConvocatoriasSena.models import Convocatia,TipoConvocatoria
+from django.db import Error,transaction
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from datetime import datetime
+
+formato_fecha="%Y-%m-%d %H:%M:%S"
+
+csrf_exempt
+def addConvocatoria(request):
+    try:
+        nombre: request.POST.get("txtNombre")
+        idTipo = request.POST.get("cbTipoConvocatoria")
+        tipo = TipoConvocatoria.objects.get(pk=idTipo)
+        cantidadBeneficiarios = request.POST.get("txtCantidadBeneficiarios")
+        fechaInicio = request.POST.get("txtFechaInicio")
+        fecha_inicio = datetime.strptime(fechaInicio,formato_fecha)
+        fechaFin = request.POST.get("txtFechaFin")
+        fecha_fin = datetime.strptime(fechaFin,formato_fecha)
+        documento = request.FILES.get("fileDocumento")
+
+        convocatoria= Convocatoria(
+            conNombre = nombre,
+            conTipo = tipo,
+            conCantidadBeneficiarios = cantidadBeneficiarios,
+            conFechaInicio = fecha_inicio,
+            conFechaFin = fecha_fin,
+            conDocumento = documento
+        )
