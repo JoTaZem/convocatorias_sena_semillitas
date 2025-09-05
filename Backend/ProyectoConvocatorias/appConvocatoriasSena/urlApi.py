389c748e50758be9ef7ae7251f414b545644d083
JoTaZem <jhonfredyzema2@gmail.com>
2025-09-05 14:18:53 -0500

    changes to maubry

--- Backend/ProyectoConvocatorias/appConvocatoriasSena/urlApi.py
+++ Backend/ProyectoConvocatorias/appConvocatoriasSena/urlApi.py
@@ -8,3 +8,13 @@ urlpatterns=[
     path("convocatoria/<int:pk>/", ConvocatoriaDetail.as_view()),
     path ("docs/",include_docs_urls(title="Documentacion Api")),
 ]
+from django.contrib import admin
+from django.urls import path
+from viewsApi import *
+from rest_framework.documentation import include_docs_urls
+
+urlpatterns=[
+    path("convocatoria/", ConvocatoriaList.as_view()),
+    path("convocatoria/<int:pk>/", ConvocatoriaDetail.as_view()),
+    path ("docs/",include_docs_urls(title="Documentacion Api")),
+]
