d1fdbc1212c4229c2dab0cf3383d2eb2586137bd
JoTaZem <jhonfredyzema2@gmail.com>
2025-09-05 13:40:28 -0500

    cambios para maubry

--- Backend/ProyectoConvocatorias/appConvocatoriasSena/urls.py
+++ Backend/ProyectoConvocatorias/appConvocatoriasSena/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from . import viewsLider, views, viewsFuncionarios
+from . import viewsLider, views, viewsFuncionarios,viewsAprendiz
 
 urlpatterns = [
     # path("", views.home),
     path("addConvocatoria/", viewsLider.addConvocatoria, name="addConvocatoria"),
     path("addFuncionario/", viewsFuncionarios.addFuncionario, name="addFuncionario"),
+    path("addAprendiz/",viewsAprendiz.addAprendiz,name="addAprendiz")
 ]
