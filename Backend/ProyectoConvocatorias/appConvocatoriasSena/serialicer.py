20f06e87f327ed3af20c9a5e9332992053521792
alejandroabc321 <alejandroabc321@gmai.com>
2025-09-05 16:53:22 -0500

    arregnado...

--- Backend/ProyectoConvocatorias/appConvocatoriasSena/serialicer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework  import serializers
-from .models import Convocatoria
-
-class convocatoriaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Convocatoria
-        fields = '__all__'
-        depth = 1 # par poder relacionarlos 
